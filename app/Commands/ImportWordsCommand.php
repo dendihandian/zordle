1fff76372b35f5785153f71ba637cae8aecae9d0
Dendi Handian <contact.dendi@yahoo.com>
2022-03-13 00:23:39 +0700

    import words command, migrations, and words_en.txt

---
+++ app/Commands/ImportWordsCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\DB;
+use LaravelZero\Framework\Commands\Command;
+
+class ImportWordsCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'import';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Command to import text contains words into database';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('importing...');
+
+        $file = fopen("words_en.txt","r");
+        while(! feof($file)){
+            $word = fgets($file);
+            DB::table('words')->insert(['word' => $word]);
+        }
+        fclose($file);
+
+        $this->info('done');
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
