7615d7bc1e92c460a227d9aca4e305ddd04ccb34
Dendi Handian <contact.dendi@yahoo.com>
2022-03-13 02:47:51 +0700

    termwind test command

---
+++ resources/views/board.blade.php
@@ -0,0 +1,48 @@
+<div>
+    <table>
+        <tbody class="text-white">
+            <tr class="text-white">
+                <td class="text-white bg-green-500 text-center">A</td>
+                <td class="text-white bg-green-500 text-center">B</td>
+                <td class="text-white bg-green-500 text-center">C</td>
+                <td class="text-white bg-green-500 text-center">D</td>
+                <td class="text-white bg-green-500 text-center">E</td>
+            </tr>
+            <tr class="text-white">
+                <td class="text-white bg-green-500 text-center">A</td>
+                <td class="text-white bg-green-500 text-center">B</td>
+                <td class="text-white bg-green-500 text-center">C</td>
+                <td class="text-white bg-green-500 text-center">D</td>
+                <td class="text-white bg-green-500 text-center">E</td>
+            </tr>
+            <tr class="text-white">
+                <td class="text-white bg-green-500 text-center">A</td>
+                <td class="text-white bg-green-500 text-center">B</td>
+                <td class="text-white bg-yellow-600 text-center text-black">C</td>
+                <td class="text-white bg-green-500 text-center">D</td>
+                <td class="text-white bg-green-500 text-center">E</td>
+            </tr>
+            <tr class="text-white">
+                <td class="text-white bg-green-500 text-center">A</td>
+                <td class="text-white bg-green-500 text-center">B</td>
+                <td class="text-white bg-green-500 text-center">C</td>
+                <td class="text-white bg-green-500 text-center">D</td>
+                <td class="text-white bg-green-500 text-center">E</td>
+            </tr>
+            <tr class="text-white">
+                <td class="text-white bg-green-500 text-center">A</td>
+                <td class="text-white bg-green-500 text-center">B</td>
+                <td class="text-white bg-green-500 text-center">C</td>
+                <td class="text-white bg-green-500 text-center">D</td>
+                <td class="text-white bg-green-500 text-center">E</td>
+            </tr>
+            <tr class="text-white">
+                <td class="text-white text-center">-</td>
+                <td class="text-white text-center">-</td>
+                <td class="text-white text-center">-</td>
+                <td class="text-white text-center">-</td>
+                <td class="text-white text-center">-</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
